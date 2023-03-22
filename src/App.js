2c64537be0d12362109dd578dfacb5f5bd8b1b68
Luan Mendes <familia.martinez.2009@hotmail.com>
2023-03-22 15:58:45 -0300

    Bootstrap Implementação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+// MEUS CSS
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// MEUS IMPORTS
+import { BrowserRouter, Routes, Route, Navigate, Router } from 'react-router-dom';
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <div className='container'>
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/about" element={ <About /> } />
+            <Route path="/login" element={ <Login /> } />
+            <Route path="/register" element={ <Register /> } />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjHUrsn2m1qNg2R6ZDlJQSYPa7K7KZKM4",
+  authDomain: "blogzinho-84061.firebaseapp.com",
+  projectId: "blogzinho-84061",
+  storageBucket: "blogzinho-84061.appspot.com",
+  messagingSenderId: "157858839417",
+  appId: "1:157858839417:web:36215151a5d82bb6a90da8"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
